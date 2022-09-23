e6549992e6b732f5a0e6cc7330fd70b70491cf36
Deywerson Pereira <deywerson.pereira@gmail.com>
2022-09-23 18:09:48 -0300

    fixed responsive from dashboard page

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -70,7 +70,7 @@ const Dashboard: NextPage = () => {
 
         <SimpleGrid flex="1" gap="4" minChildWidth="320px" alignItems="flex-start">
           <Box
-            p="8"
+            p={["6", "8"]}
             bg="gray.800"
             borderRadius={8}
             pb="4"
@@ -81,7 +81,7 @@ const Dashboard: NextPage = () => {
             <Chart options={options} series={series} type="area" height={160} />
           </Box>
           <Box
-            p="8"
+            p={["6", "8"]}
             bg="gray.800"
             borderRadius={8}
             pb="4"
