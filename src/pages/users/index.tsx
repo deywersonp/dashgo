a8a2a8a15db6c6b9b4114ac9d4e24c012adefa9d
Deywerson Pereira <deywerson.pereira@gmail.com>
2022-09-23 18:23:27 -0300

    added responsive at users list page and Pagination component

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,11 +1,16 @@
 import type { NextPage } from 'next';
-import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr } from '@chakra-ui/react';
+import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from '@chakra-ui/react';
 import { Header } from '../../components/Header';
 import { Sidebar } from '../../components/Sidebar';
 import { RiAddLine, RiPencilLine } from 'react-icons/ri';
 import { Pagination } from '../../components/Pagination';
 
 const UsersList: NextPage = () => {
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    lg: true,
+  });
+
   return (
     <Box>
       <Header />
@@ -51,17 +56,17 @@ const UsersList: NextPage = () => {
           <Table colorScheme="whiteAlpha">
             <Thead>
               <Tr>
-                <Th px="6" color="gray.300" width="8">
+                <Th px={["4", "4", "6"]} color="gray.300" width="8">
                   <Checkbox colorScheme="pink" />
                 </Th>
                 <Th>Usuário</Th>
-                <Th>Data de Cadastro</Th>
-                <Th width="0" />
+                {isWideVersion && <Th>Data de Cadastro</Th>}
+                {isWideVersion && <Th width="0" />}
               </Tr>
             </Thead>
             <Tbody>
               <Tr>
-                <Td px="6">
+                <Td px={["4", "4", "6"]}>
                   <Checkbox colorScheme="pink" />
                 </Td>
                 <Td>
@@ -70,21 +75,23 @@ const UsersList: NextPage = () => {
                     <Text fontSize="sm" color="gray.300">deywerson.pereira@gmail.com</Text>
                   </Box>
                 </Td>
-                <Td>21 de Setembro, 2022</Td>
-                <Td>
-                  <Button
-                    as="a"
-                    size="sm"
-                    fontSize="sm"
-                    colorScheme="purple"
-                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                  >
-                    Editar
-                  </Button>
-                </Td>
+                {isWideVersion && <Td>21 de Setembro, 2022</Td>}
+                {isWideVersion && (
+                  <Td>
+                    <Button
+                      as="a"
+                      size="sm"
+                      fontSize="sm"
+                      colorScheme="purple"
+                      leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                    >
+                      {isWideVersion ? 'Editar' : ''}
+                    </Button>
+                  </Td>
+                )}
               </Tr>
               <Tr>
-                <Td px="6">
+                <Td px={["4", "4", "6"]}>
                   <Checkbox colorScheme="pink" />
                 </Td>
                 <Td>
@@ -93,21 +100,23 @@ const UsersList: NextPage = () => {
                     <Text fontSize="sm" color="gray.300">deywerson.pereira@gmail.com</Text>
                   </Box>
                 </Td>
-                <Td>21 de Setembro, 2022</Td>
-                <Td>
-                  <Button
-                    as="a"
-                    size="sm"
-                    fontSize="sm"
-                    colorScheme="purple"
-                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                  >
-                    Editar
-                  </Button>
-                </Td>
+                {isWideVersion && <Td>21 de Setembro, 2022</Td>}
+                {isWideVersion && (
+                  <Td>
+                    <Button
+                      as="a"
+                      size="sm"
+                      fontSize="sm"
+                      colorScheme="purple"
+                      leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                    >
+                      {isWideVersion ? 'Editar' : ''}
+                    </Button>
+                  </Td>
+                )}
               </Tr>
               <Tr>
-                <Td px="6">
+                <Td px={["4", "4", "6"]}>
                   <Checkbox colorScheme="pink" />
                 </Td>
                 <Td>
@@ -116,18 +125,20 @@ const UsersList: NextPage = () => {
                     <Text fontSize="sm" color="gray.300">deywerson.pereira@gmail.com</Text>
                   </Box>
                 </Td>
-                <Td>21 de Setembro, 2022</Td>
-                <Td>
-                  <Button
-                    as="a"
-                    size="sm"
-                    fontSize="sm"
-                    colorScheme="purple"
-                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                  >
-                    Editar
-                  </Button>
-                </Td>
+                {isWideVersion && <Td>21 de Setembro, 2022</Td>}
+                {isWideVersion && (
+                  <Td>
+                    <Button
+                      as="a"
+                      size="sm"
+                      fontSize="sm"
+                      colorScheme="purple"
+                      leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                    >
+                      {isWideVersion ? 'Editar' : ''}
+                    </Button>
+                  </Td>
+                )}
               </Tr>
             </Tbody>
           </Table>
