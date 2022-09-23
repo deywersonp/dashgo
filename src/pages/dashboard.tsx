7d5e0548bc90bd32d0812b761e5f571696644da2
Deywerson Pereira <deywerson.pereira@gmail.com>
2022-09-23 19:10:07 -0300

    hotfix: Chart oversizing container width

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import type { NextPage } from 'next';
 import dynamic from 'next/dynamic';
 import { Box, Flex, SimpleGrid, Text, theme } from '@chakra-ui/react';
@@ -61,6 +62,12 @@ const series = [
 ];
 
 const Dashboard: NextPage = () => {
+  const [assembleGraphics, setAssembleGraphics] = useState(false);
+
+  useEffect(() => {
+    setAssembleGraphics(true);
+  }, []);
+
   return (
     <Flex direction="column" h="100vh">
       <Header />
@@ -68,30 +75,33 @@ const Dashboard: NextPage = () => {
       <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
         <Sidebar />
 
-        <SimpleGrid flex="1" gap="4" minChildWidth="320px" alignItems="flex-start">
-          <Box
-            p={["6", "8"]}
-            bg="gray.800"
-            borderRadius={8}
-            pb="4"
-          >
-            <Text fontSize="lg" mb="4">
-              Inscritos da Semana
-            </Text>
-            <Chart options={options} series={series} type="area" height={160} />
-          </Box>
-          <Box
-            p={["6", "8"]}
-            bg="gray.800"
-            borderRadius={8}
-            pb="4"
-          >
-            <Text fontSize="lg" mb="4">
-              Taxa de Abertura
-            </Text>
-            <Chart options={options} series={series} type="area" height={160} />
-          </Box>
-        </SimpleGrid>
+        {assembleGraphics && (
+          <SimpleGrid flex="1" gap="4" minChildWidth="320px" alignItems="flex-start">
+            <Box
+              p={["6", "8"]}
+              bg="gray.800"
+              borderRadius={8}
+              pb="4"
+            >
+              <Text fontSize="lg" mb="4">
+                Inscritos da Semana
+              </Text>
+              <Chart options={options} series={series} type="area" height={160} />
+            </Box>
+            <Box
+              p={["6", "8"]}
+              bg="gray.800"
+              borderRadius={8}
+              pb="4"
+            >
+              <Text fontSize="lg" mb="4">
+                Taxa de Abertura
+              </Text>
+              <Chart options={options} series={series} type="area" height={160} />
+            </Box>
+          </SimpleGrid>
+        )}
+
       </Flex>
     </Flex>
   )
