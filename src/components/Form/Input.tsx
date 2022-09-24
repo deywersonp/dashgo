be48fd9b975814ce76e8e1085e6edc1f5385c19e
Deywerson Pereira <deywerson.pereira@gmail.com>
2022-09-23 21:03:16 -0300

    added react-hook-form / added forwardRef at Input component

--- src/components/Form/Input.tsx
+++ src/components/Form/Input.tsx
@@ -2,31 +2,36 @@ import {
   Input as ChakraInput,
   InputProps as ChakraInputProps,
   FormLabel,
-  FormControl
+  FormControl,
 } from '@chakra-ui/react';
+import { forwardRef, ForwardRefRenderFunction } from 'react';
 
 interface InputProps extends ChakraInputProps {
   name: string;
   label?: string;
 };
 
-export const Input = ({ name, label, ...rest }: InputProps) => {
-  return (
-    <FormControl>
-      {!!label && <FormLabel htmlFor={name}>{label}</FormLabel>}
+const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps>
+  = ({ name, label, ...rest }, ref) => {
+    return (
+      <FormControl>
+        {!!label && <FormLabel htmlFor={name}>{label}</FormLabel>}
 
-      <ChakraInput
-        id={name}
-        name={name}
-        focusBorderColor="pink.500"
-        bgColor="gray.900"
-        variant="filled"
-        _hover={{
-          bgColor: 'gray.900'
-        }}
-        size="lg"
-        {...rest}
-      />
-    </FormControl>
-  )
-};
+        <ChakraInput
+          id={name}
+          name={name}
+          focusBorderColor="pink.500"
+          bgColor="gray.900"
+          variant="filled"
+          _hover={{
+            bgColor: 'gray.900'
+          }}
+          size="lg"
+          ref={ref}
+          {...rest}
+        />
+      </FormControl>
+    )
+  };
+
+export const Input = forwardRef(InputBase);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,21 @@
 import type { NextPage } from 'next';
 import { Flex, Button, Stack } from '@chakra-ui/react';
+import { useForm, SubmitHandler } from 'react-hook-form';
 import { Input } from '../components/Form/Input';
 
+type SignInFormData = {
+  email: string;
+  password: string;
+};
+
 const Home: NextPage = () => {
+  const { register, handleSubmit, formState } = useForm<SignInFormData>();
+
+  const handleSignIn: SubmitHandler<SignInFormData> = async (values) => {
+    await new Promise(resolve => setTimeout(resolve, 2000));
+    console.log(values);
+  };
+
   return (
     <Flex
       w="100vw"
@@ -18,10 +31,11 @@ const Home: NextPage = () => {
         p="8"
         borderRadius={8}
         flexDir="column"
+        onSubmit={handleSubmit(handleSignIn)}
       >
         <Stack spacing="4">
-          <Input name="email" type="email" label="E-mail" />
-          <Input name="password" type="password" label="Senha" />
+          <Input type="email" label="E-mail" {...register("email")} />
+          <Input type="password" label="Senha" {...register("password")} />
         </Stack>
 
         <Button
@@ -29,6 +43,7 @@ const Home: NextPage = () => {
           mt="6"
           colorScheme="pink"
           size="lg"
+          isLoading={formState.isSubmitting}
         >
           Entrar
         </Button>
